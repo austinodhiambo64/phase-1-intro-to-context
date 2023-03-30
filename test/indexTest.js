0f9b57b4469d4c4b38a5036d327deee70fe77951
Austin <austinodhiambo64@gmail.com>
2023-03-30 20:29:34 +0300

    Assignment Completed

--- test/indexTest.js
+++ test/indexTest.js
@@ -11,7 +11,89 @@ describe("The payroll system", function () {
       })
 
       it("populates a familyName field from the 1th element", function () {
-        let testEmployee = createEmployeeRecord(["Gray", "Worm", "Security", 1])
+        let te// Your code here
+        let createEmployeeRecord = function(row){
+            return {
+                firstName: row[0],
+                familyName: row[1],
+                title: row[2],
+                payPerHour: row[3],
+                timeInEvents: [],
+                timeOutEvents: []
+            }
+        }
+        
+        let createEmployeeRecords = function(employeeRowData) {
+            return employeeRowData.map(function(row){
+                return createEmployeeRecord(row)
+            })
+        }
+        
+        let createTimeInEvent = function(employee, dateStamp){
+            let [date, hour] = dateStamp.split(' ')
+        
+            employee.timeInEvents.push({
+                type: "TimeIn",
+                hour: parseInt(hour, 10),
+                date,
+            })
+        
+            return employee
+        }
+        
+        let createTimeOutEvent = function(employee, dateStamp){
+            let [date, hour] = dateStamp.split(' ')
+        
+            employee.timeOutEvents.push({
+                type: "TimeOut",
+                hour: parseInt(hour, 10),
+                date,
+            })
+        
+            return employee
+        }
+        
+        let hoursWorkedOnDate = function(employee, soughtDate){
+            let inEvent = employee.timeInEvents.find(function(e){
+                return e.date === soughtDate
+            })
+        
+            let outEvent = employee.timeOutEvents.find(function(e){
+                return e.date === soughtDate
+            })
+        
+            return (outEvent.hour - inEvent.hour) / 100
+        }
+        
+        let wagesEarnedOnDate = function(employee, dateSought){
+            let rawWage = hoursWorkedOnDate(employee, dateSought)
+                * employee.payPerHour
+            return parseFloat(rawWage.toString())
+        }
+        
+        let allWagesFor = function(employee){
+            let eligibleDates = employee.timeInEvents.map(function(e){
+                return e.date
+            })
+        
+            let payable = eligibleDates.reduce(function(memo, d){
+                return memo + wagesEarnedOnDate(employee, d)
+            }, 0)
+        
+            return payable
+        }
+        
+        let findEmployeeByFirstName = function(srcArray, firstName) {
+          return srcArray.find(function(rec){
+            return rec.firstName === firstName
+          })
+        }
+        
+        let calculatePayroll = function(arrayOfEmployeeRecords){
+            return arrayOfEmployeeRecords.reduce(function(memo, rec){
+                return memo + allWagesFor(rec)
+            }, 0)
+        }stEmployee = createEmployeeRecord(["Gray", "Worm", "Security", 1])
         expect(testEmployee.familyName).to.eq("Worm")
       })
 
